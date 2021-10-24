b098f453cab5bad16d48e0c5f02745285c2933aa
AlexSerebro <88261876+AlexSerebro@users.noreply.github.com>
2021-10-24 10:06:59 +0000

    Deploying to gh-pages from @ AlexSerebro/comand-prodject@be19deeaa42679a4e8d415125023940236ae74cf 🚀

--- src.349c2bf6.js
+++ src.349c2bf6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/comand-prodject/src.5996cbd5.js.map
+//# sourceMappingURL=/comand-prodject/src.349c2bf6.js.map
